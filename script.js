cd791fa97d52d2ebfc1e0dad66bd3ba075d137bc
sanikapatil30 <sanikap5998@gmail.com>
2026-01-31 22:44:02 +0530

    TASK-04-Weather app development

--- script.js
+++ script.js
@@ -1,61 +1,38 @@
-let startTime;
-let elapsedTime = 0;
-let timerInterval;
-let running = false;
-let lapCount = 1;
+const apiKey = "25957b5ea53dd9a14de638ede2d751f3";
 
-const display = document.getElementById("display");
-const startBtn = document.getElementById("start");
-const resetBtn = document.getElementById("reset");
-const lapBtn = document.getElementById("lap");
-const lapList = document.getElementById("lapList");
+function getWeather() {
+    const city = document.getElementById("cityInput").value.trim();
 
-function formatTime(ms) {
-  let seconds = Math.floor(ms / 1000);
-  let minutes = Math.floor(seconds / 60);
-  let hours = Math.floor(minutes / 60);
+    if (city === "") {
+        alert("Please enter city name");
+        return;
+    }
 
-  seconds %= 60;
-  minutes %= 60;
+    const url =
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    fetch(url)
+        .then(res => {
+            console.log("STATUS:", res.status);
+            if (res.status === 401) {
+                throw new Error("Unauthorized - API key issue");
+            }
+            if (!res.ok) {
+                throw new Error("City not found");
+            }
+            return res.json();
+        })
+        .then(data => {
+            document.getElementById("city").innerText = data.name;
+            document.getElementById("temp").innerText = `🌡 ${data.main.temp} °C`;
+            document.getElementById("desc").innerText = data.weather[0].description;
+            document.getElementById("humidity").innerText = `💧 ${data.main.humidity}%`;
+            document.getElementById("wind").innerText = `🌬 ${data.wind.speed} m/s`;
+            document.getElementById("icon").src =
+              `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        })
+        .catch(err => {
+            console.error(err.message);
+            alert(err.message);
+        });
 }
-
-function startStopwatch() {
-  if (!running) {
-    startTime = Date.now() - elapsedTime;
-    timerInterval = setInterval(() => {
-      elapsedTime = Date.now() - startTime;
-      display.textContent = formatTime(elapsedTime);
-    }, 1000);
-
-    startBtn.textContent = "Pause";
-    running = true;
-  } else {
-    clearInterval(timerInterval);
-    startBtn.textContent = "Start";
-    running = false;
-  }
-}
-
-function resetStopwatch() {
-  clearInterval(timerInterval);
-  elapsedTime = 0;
-  running = false;
-  lapCount = 1;
-  display.textContent = "00:00:00";
-  startBtn.textContent = "Start";
-  lapList.innerHTML = "";
-}
-
-function addLap() {
-  if (running) {
-    const li = document.createElement("li");
-    li.textContent = `Lap ${lapCount++} - ${formatTime(elapsedTime)}`;
-    lapList.appendChild(li);
-  }
-}
-
-startBtn.addEventListener("click", startStopwatch);
-resetBtn.addEventListener("click", resetStopwatch);
-lapBtn.addEventListener("click", addLap);
